53dafa5e6f398ed470db27696e8b7b9795672335
Max Davidov <m.k.davydov@tinkoff.ru>
2020-07-25 14:08:34 +0300

    :bug: bind knex to Objection model

--- auth/src/start.ts
+++ auth/src/start.ts
@@ -1,5 +1,9 @@
+import { Model } from 'objection'
 import { ENV } from './config/constants'
+import { knex } from './database/knex'
 import { server } from './server/server'
+
+Model.knex(knex)
 
 server.listen({ port: ENV.PORT }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
