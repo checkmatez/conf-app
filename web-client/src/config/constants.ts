fd0e752756fe530bf0770fecea74c0631902705a
Max Davidov <m.k.davydov@tinkoff.ru>
2020-08-02 16:22:56 +0300

    ☸️add ingress, dockerize web-client

--- web-client/src/config/constants.ts
+++ web-client/src/config/constants.ts
@@ -2,6 +2,12 @@ export const ACCESS_TOKEN_KEY = 'access-token'
 
 export const REFRESH_TOKEN_KEY = 'refresh-token'
 
-export const AUTH_GRAPHQL_URI = 'http://localhost:4070'
+export const AUTH_GRAPHQL_URI =
+  process.env.NODE_ENV === 'production' ? '/api/auth' : 'http://localhost:4070'
 
-export const CHAT_GRAPHQL_WEBSOCKET = 'ws://localhost:4071'
+export const CHAT_GRAPHQL_WEBSOCKET =
+  process.env.NODE_ENV === 'production'
+    ? window.location.protocol === 'https:'
+      ? `wss://${window.location.host}/api/chat`
+      : `ws://${window.location.host}/api/chat`
+    : 'ws://localhost:4071'
