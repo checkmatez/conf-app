b55c9e4d56c6af46d0e26133a294e400dbb30784
Max Davidov <m.k.davydov@tinkoff.ru>
2020-08-09 14:10:50 +0300

    :bug: fix endpoints

--- web-client/src/config/constants.ts
+++ web-client/src/config/constants.ts
@@ -2,18 +2,19 @@ export const ACCESS_TOKEN_KEY = 'access-token'
 
 export const REFRESH_TOKEN_KEY = 'refresh-token'
 
-// export const AUTH_GRAPHQL_URI =
-//   process.env.NODE_ENV === 'production' ? '/api/auth' : 'http://localhost:4070'
+export const AUTH_GRAPHQL_URI =
+  process.env.NODE_ENV === 'production' ? '/api/auth' : 'http://localhost:4070'
 
-// export const CHAT_GRAPHQL_WEBSOCKET =
-//   process.env.NODE_ENV === 'production'
-//     ? window.location.protocol === 'https:'
-//       ? `wss://${window.location.host}/api/chat`
-//       : `ws://${window.location.host}/api/chat`
-//     : 'ws://localhost:4071'
+export const CHAT_GRAPHQL_URI =
+  process.env.NODE_ENV === 'production' ? '/api/chat' : 'http://localhost:4071'
 
-// export const AUTH_GRAPHQL_URI = 'http://139.59.206.240/api/auth'
+export const CHAT_GRAPHQL_WEBSOCKET =
+  process.env.NODE_ENV === 'production'
+    ? window.location.protocol === 'https:'
+      ? `wss://${window.location.host}/api/chat`
+      : `ws://${window.location.host}/api/chat`
+    : 'ws://localhost:4071'
 
-export const AUTH_GRAPHQL_URI = 'http://localhost:4070'
-export const CHAT_GRAPHQL_URI = 'http://localhost:4071'
-export const CHAT_GRAPHQL_WEBSOCKET = 'ws://localhost:4071'
+// export const AUTH_GRAPHQL_URI = 'http://localhost:4070'
+// export const CHAT_GRAPHQL_URI = 'http://localhost:4071'
+// export const CHAT_GRAPHQL_WEBSOCKET = 'ws://localhost:4071'
